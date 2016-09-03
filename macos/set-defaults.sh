669f5ffd7e7d1fb1163f417541275a1e5afe400c
Marat Tukhvatullin <marat.tukhvatullin@cinarra.com>
2016-09-03 22:54:37 +0300

    Create set-defaults.sh

---
+++ macos/set-defaults.sh
@@ -0,0 +1,4 @@
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
