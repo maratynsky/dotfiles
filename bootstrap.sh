20ffad016076795893a7ff279308f788d52e217a
Marat Tukhvatullin <maratynsky@marats-mbp.localdomain>
2019-08-30 00:01:46 +0200

    Fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 # vim
+mkdir -p "~/.vim/colors"
 ln -s "$(pwd)/vim/monochrome.vim" ~/.vim/colors/monochrome.vim
 ln -s "$(pwd)/vim/vimrc" ~/.vimrc
 # macos
 bash macos/set-defaults.sh
 # bash
 ln -s "$(pwd)/bash/bash_profile" ~/.bash_profile_prompt
-echo "source ~/.bash_profile_prompt" > ~/.bash_profile
+echo "source ~/.bash_profile_prompt" >> ~/.bash_profile
