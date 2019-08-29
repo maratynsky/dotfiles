9c198a0166842c76095a40262d5f580db112db42
Marat Tukhvatullin <marat.tukhvatullin@freshcells.de>
2019-08-29 16:55:10 +0200

    Everything is monochrome

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+# vim
+ln -s "$(pwd)/vim/monochrome.vim" ~/.vim/colors/monochrome.vim
+ln -s "$(pwd)/vim/vimrc" ~/.vimrc
+# macos
+bash macos/set-defaults.sh
+# bash
+ln -s "$(pwd)/bash/bash_profile" ~/.bash_profile_prompt
+echo "source ~/.bash_profile_prompt" > ~/.bash_profile
